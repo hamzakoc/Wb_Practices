08388b059e377ef214369733a98c404cfa66b123
Hamza Koc <hamzakooc@gmail.com>
2021-04-26 14:02:12 -0400

    Add files via upload

---
+++ wk1/app.js
@@ -0,0 +1,25 @@
+ //3 ****************
+ let firstName = "Hamza";
+ //4 ****************
+ let lastName = "Koc";
+ //5 ****************
+ let myAge = 31;
+ //6 ****************
+ let counter = 0;
+ //7 ****************
+ alert(`My name is ${firstName} ${lastName}`)
+ counter++;
+ //8 ****************************
+ console.log(`counter: ${counter}`); 
+ counter++;
+ //9 *******************************
+ document.write(`<h1>Name ${firstName} ${lastName}</h1>`);
+ document.write(`<p>My student ID is :  101274109</p>`);
+ counter++;
+ //10*******************************
+ let userInput =Number( prompt("What do you think the value of counter variable is?"));
+ counter++;
+ //11*******************************
+ document.write(`<p>You entered: ${userInput}</p>`);
+ document.write(`<p>Counter: ${counter}</p>`);
+ document.write(`<p>userInput - counter: ${userInput-counter}</p>`);
